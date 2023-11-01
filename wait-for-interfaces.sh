1be39b4fce2c117fca80eccdaaf30c337f1aa13b
OleHolmNielsen <Ole.H.Nielsen@fysik.dtu.dk>
2023-11-01 16:19:01 +0100

    Reordered the loop

--- wait-for-interfaces.sh
+++ wait-for-interfaces.sh
@@ -3,18 +3,22 @@
 TIMEOUT=300
 INTERFACES=("ib0")
 
+echo "Wait for network devices, available connections are:"
+nmcli connection show 
+
 for IFACE in "${INTERFACES[@]}"; do
     COUNTER=0
-    while ! nmcli --get-values GENERAL.STATE device show "$IFACE" | grep -q "(connected)"; do
-        # Print debugging info to syslog
-        echo -n "Waiting for interface $IFACE to come online:"
-        nmcli --get-values GENERAL.STATE device show "$IFACE"
-        sleep 1
+    status="`nmcli --get-values GENERAL.STATE device show $IFACE`"
+    until echo "$status" | grep -q "(connected)"
+    do
         COUNTER=$((COUNTER + 1))
-        if [ "$COUNTER" -ge "$TIMEOUT" ]; then
+        if [ "$COUNTER" -gt "$TIMEOUT" ]; then
             echo "ERROR: Interface $IFACE did not come online within timeout=$TIMEOUT"
             exit 1
         fi
+        sleep 1
+	status="`nmcli --get-values GENERAL.STATE device show $IFACE`"
+        echo "Waiting for interface $IFACE to come online: $status"
     done
 done
 
