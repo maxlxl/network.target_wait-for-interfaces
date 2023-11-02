0e2ba07b4f3c78566e8c9bbfec1ed8f13c26c377
OleHolmNielsen <Ole.H.Nielsen@fysik.dtu.dk>
2023-11-02 13:13:38 +0100

    Add check for $IFACE existing

--- wait-for-interfaces.sh
+++ wait-for-interfaces.sh
@@ -3,23 +3,27 @@
 TIMEOUT=300
 INTERFACES=("ib0")
 
-echo "Wait for network devices, available connections are:"
-nmcli connection show 
-
 for IFACE in "${INTERFACES[@]}"; do
-    COUNTER=0
-    status="`nmcli --get-values GENERAL.STATE device show $IFACE`"
-    until echo "$status" | grep -q "(connected)"
-    do
-        COUNTER=$((COUNTER + 1))
-        if [ "$COUNTER" -gt "$TIMEOUT" ]; then
-            echo "ERROR: Interface $IFACE did not come online within timeout=$TIMEOUT"
-            exit 1
-        fi
-        sleep 1
-	status="`nmcli --get-values GENERAL.STATE device show $IFACE`"
-        echo "Waiting for interface $IFACE to come online: $status"
-    done
+    if `nmcli connection show | awk '{print $NF}' | grep -q $IFACE`
+    then
+        echo "Wait for network interface $IFACE to come online"
+        COUNTER=0
+        status="`nmcli --get-values GENERAL.STATE device show $IFACE`"
+        until echo "$status" | grep -q "(connected)"
+        do
+            COUNTER=$((COUNTER + 1))
+            if [ "$COUNTER" -gt "$TIMEOUT" ]; then
+                echo "ERROR: Interface $IFACE did not come online within timeout=$TIMEOUT"
+                exit 1
+            fi
+            sleep 1
+	    status="`nmcli --get-values GENERAL.STATE device show $IFACE`"
+            echo "Waiting for interface $IFACE to come online: $status"
+        done
+    else
+        echo "Notice: Interface $IFACE not found, available connections are:"
+        nmcli connection show 
+    fi
 done
 
 exit 0
